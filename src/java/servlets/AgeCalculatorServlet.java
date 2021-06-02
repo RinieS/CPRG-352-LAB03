a80285d20a5bd83fc49096137f75bb2b7ad6109b
840979 <840979@SAIT227580IT.ACDM.DS.SAIT.CA>
2021-06-02 06:25:23 -0600

    Second commit Created ageCalculator forms and servlets pkg w/ AgeCalculatorServlet.java

---
+++ src/java/servlets/AgeCalculatorServlet.java
@@ -0,0 +1,57 @@
+
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 840979
+ */
+public class AgeCalculatorServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+  
+   
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+      
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ web/WEB-INF/ageCalculator.jsp
@@ -0,0 +1,23 @@
+<%-- 
+    Document   : ageCalculator
+    Created on : Jun 2, 2021, 6:04:45 AM
+    Author     : 840979
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Age Calculator</title>
+    </head>
+    <body>
+        <h1>Age Calculator</h1>
+         <form method="post" action="hello">
+            <label>Enter your age:</label>
+            <input type="text" name="age" value="" placeholder="35">
+            <br>
+            <input type="submit" value="Age next birthday">
+        </form>
+    </body>
+</html>
